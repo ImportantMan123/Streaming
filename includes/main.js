1c7e0db0084061d294f7a6b57abfeaa00480b22c
Jack Cowling <jackcowling@Jacks-MacBook-Pro.local>
2019-06-07 11:06:47 +0100

    javascript code has changed & returns no error code, however it doesn't yet copy the last child of 'project-cont-btm' as expected

--- includes/main.js
+++ includes/main.js
@@ -1,12 +1,12 @@
 var trackLinkArray = ["streaming/tracks/01-Elimination_Chamber.mp3", "streaming/tracks/02-The-Daily-News.mp3", "streaming/tracks/03-Gamebreaker.mp3", "streaming/tracks/04-Between-Villains.mp3", "streaming/tracks/05-45.mp3", "streaming/tracks/06-Balance.mp3", "streaming/tracks/07-Nebraska.mp3", "streaming/tracks/08-Quest-Power.mp3", "streaming/tracks/09-Wind-In-My-Sails.mp3", "streaming/tracks/10-Warlord-Leather.mp3", "streaming/tracks/11-Knucklehead.mp3", "streaming/tracks/12-Bill.mp3"];
 
 
-var trackCont = document.getElementById("project-track-cont");
-
-trackLinkArray.forEach (function(entry) {
-    console.log(entry);
+trackLinkArray.forEach (function(element) {
+    myFunction(element);
 });
 
-var itm = document.getElementsByID("project-cont-btm").lastChild;
-var cln = itm.cloneNode(true);
-document.getElementById("project-cont-btm").appendChild(cln);
+function myFunction() {
+    const itm = document.getElementById("project-cont-btm").lastChild;
+    const cln = itm.cloneNode(true);
+    document.getElementById("project-cont-btm").appendChild(cln);
+}
