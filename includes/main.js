abd92dda71ebbf92c7375c000378331dd4108bda
Jack Cowling <jackcowling@Jacks-MacBook-Pro.local>
2019-06-11 22:00:46 +0100

    The play button now works & get's it's source from a variable.

--- includes/main.js
+++ includes/main.js
@@ -1,9 +1,12 @@
-const trackLinkArray = ["streaming/tracks/01-Elimination_Chamber.mp3", "streaming/tracks/02-The-Daily-News.mp3", "streaming/tracks/03-Gamebreaker.mp3", "streaming/tracks/04-Between-Villains.mp3", "streaming/tracks/05-45.mp3", "streaming/tracks/06-Balance.mp3", "streaming/tracks/07-Nebraska.mp3", "streaming/tracks/08-Quest-Power.mp3", "streaming/tracks/09-Wind-In-My-Sails.mp3", "streaming/tracks/10-Warlord-Leather.mp3", "streaming/tracks/11-Knucklehead.mp3", "streaming/tracks/12-Bill.mp3"];
+const trackLinkArray = ["tracks/01-Elimination_Chamber.mp3", "tracks/02-The-Daily-News.mp3", "tracks/03-Gamebreaker.mp3", "tracks/04-Between-Villains.mp3", "tracks/05-45.mp3", "tracks/06-Balance.mp3", "tracks/07-Nebraska.mp3", "tracks/08-Quest-Power.mp3", "tracks/09-Wind-In-My-Sails.mp3", "tracks/10-Warlord-Leather.mp3", "tracks/11-Knucklehead.mp3", "tracks/12-Bill.mp3"];
 
 const projectContBtm = document.getElementById('project-cont-btm');
 var projectTrackCont = projectContBtm.lastElementChild;
 var projectTrackTitle = projectTrackCont.children[1];
 
+var currentSong = trackLinkArray[1];
+
+/* Dynamically copy & create all the track information */
 for(let i = 0; i < trackLinkArray.length; i++) {
     createProjectTrackCont(i);
 }
@@ -27,6 +30,47 @@ function createProjectTrackCont(cycleNumber) {
     }
 
 }
+
+const player = document.getElementById('music_player');
+player.setAttribute('src',currentSong);
+
+
+
+
+function play_aud() {
+    player.play();
+}
+
+function pause_aud() {
+    player.pause();
+}
+
+function stop_aud() {
+    player.pause();
+    player.currentTime = 0;
+}
+
+function change_vol() {
+    player.volume = document.getElementById("change_vol").value;
+}
+
+/*Play Functionality */
+/*let j = 0;
+var songNumber = j;
+var currentSong = trackLinkArray[songNumber];
+
+function clickPlay() {
+    currentSong
+}
+
+function clickNext() {
+    j++;
+}
+
+function clickPrev(){
+    J--;
+} */
+
 
 
 
